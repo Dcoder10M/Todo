27ff1fcff7b3016dc3332cdbb3cad7d8d18c74da
Dcoder10M <div.singh.rathore.ff@gmail.com>
2024-05-08 14:27:51 +0530

    Updated CSS

--- todo_frontend/src/components/DisplayTodos.jsx
+++ todo_frontend/src/components/DisplayTodos.jsx
@@ -4,10 +4,13 @@ function DisplayTodos({todos}) {
     <div className="roww">
       {todos.map((todo)=>(
         <div key={todo._id} className="cardd borderr col-12 col-sm-6 col-md-4 col-lg-3">
-            <h2>{todo.title}</h2>
-            <p>{todo.description}</p>
-            <button>Mark as done</button>
-        </div>
+  <div>
+    <i className="pin"></i>
+    <h2>{todo.title}</h2>
+    <p>{todo.description}</p>
+  </div>
+  <button className="btn btn-outline-secondary btn-lg btn-block">Mark as done</button>
+</div>
       ))}
     </div>
   )
